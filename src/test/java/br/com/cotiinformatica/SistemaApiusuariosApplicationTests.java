aaedee4c89362f3d19aaab30510bd1ec141e002c
davidrocha87 <david.developer27@gmail.com>
2023-07-22 15:22:48 -0300

    feat: Desenvolvimento dos serviços de produtos

--- src/test/java/br/com/cotiinformatica/SistemaApiusuariosApplicationTests.java
+++ src/test/java/br/com/cotiinformatica/SistemaApiusuariosApplicationTests.java
@@ -1,5 +1,8 @@
 package br.com.cotiinformatica;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -9,10 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import br.com.cotiinformatica.dtos.AutenticarRequestDto;
+import br.com.cotiinformatica.dtos.AutenticarResponseDto;
 import br.com.cotiinformatica.dtos.CriarContaRequestDto;
+import br.com.cotiinformatica.dtos.ProdutosGetResponseDto;
+import br.com.cotiinformatica.dtos.ProdutosPostRequestDto;
+import br.com.cotiinformatica.dtos.ProdutosPutRequestDto;
 import br.com.cotiinformatica.dtos.RecuperarSenhaRequestDto;
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,6 +34,8 @@ class SistemaApiusuariosApplicationTests {
 	//atributos
 	private static String emailUsuario;
 	private static String senhaUsuario;
+	private static String accessToken;
+	private static Integer idProduto;
 	@Test
 	@Order(1)
 	public void criarContaTest() throws Exception {
@@ -52,10 +62,16 @@ class SistemaApiusuariosApplicationTests {
 		dto.setEmail(emailUsuario);
 		dto.setSenha(senhaUsuario);
 		
-		mockMvc.perform(post("/api/usuarios/autenticar")
+		MvcResult result = mockMvc.perform(post("/api/usuarios/autenticar")
 				.contentType("application/json")
 				.content(objectMapper.writeValueAsString(dto)))
-				.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		String responseBody = result.getResponse().getContentAsString();
+		AutenticarResponseDto response = objectMapper.readValue(responseBody, AutenticarResponseDto.class);
+		
+		accessToken = response.getAccessToken();
 	}
 	@Test
 	@Order(3)
@@ -67,6 +83,66 @@ class SistemaApiusuariosApplicationTests {
 		mockMvc.perform(post("/api/usuarios/recuperar-senha")
 				.contentType("application/json")
 				.content(objectMapper.writeValueAsString(dto)))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	@Order(4)
+	public void cadastrarProdutoTest() throws Exception {
+		
+		Faker faker = new Faker();
+		
+		ProdutosPostRequestDto dto = new ProdutosPostRequestDto();
+		dto.setNome(faker.commerce().productName());
+		dto.setPreco(faker.number().randomDouble(2, 1, 1000));
+		dto.setQuantidade(faker.number().randomDigit());
+		
+		MvcResult result = mockMvc.perform(post("/api/produtos")
+				.header("Authorization", "Bearer " + accessToken)
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(dto)))
+				.andExpect(status().isCreated())
+				.andReturn();
+		
+		String responseBody = result.getResponse().getContentAsString();
+		ProdutosGetResponseDto response = objectMapper.readValue(responseBody, ProdutosGetResponseDto.class);
+		
+		idProduto = response.getIdProduto();
+	}
+	@Test
+	@Order(5)
+	public void atualizarProdutoTest() throws Exception {
+		
+		Faker faker = new Faker();
+		
+		ProdutosPutRequestDto dto = new ProdutosPutRequestDto();
+		dto.setIdProduto(idProduto);
+		dto.setNome(faker.commerce().productName());
+		dto.setPreco(faker.number().randomDouble(2, 1, 1000));
+		dto.setQuantidade(faker.number().randomDigit());
+		
+		mockMvc.perform(put("/api/produtos")
+				.header("Authorization", "Bearer " + accessToken)
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(dto)))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	@Order(6)
+	public void consultarProdutoTest() throws Exception {
+		
+		mockMvc.perform(get("/api/produtos/" + idProduto)
+				.header("Authorization", "Bearer " + accessToken))
+				.andExpect(status().isOk());		
+	}
+	
+	@Test
+	@Order(7)
+	public void excluirProdutoTest() throws Exception {
+		
+		mockMvc.perform(delete("/api/produtos/" + idProduto)
+				.header("Authorization", "Bearer " + accessToken))
 				.andExpect(status().isOk());
 	}
 }
